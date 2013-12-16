b3fe55afb7d69024ae9a6b242dde05e259eca2d5
Vladimir Kanubrikov <vkanubrikov@dataart.com>
2013-12-16 17:49:41 +0200

    sqlAlchemy

--- ChatServer/db.py
+++ ChatServer/db.py
@@ -16,6 +16,16 @@ association_room_user = Table('room_user', Base.metadata,
     Column('user_id', Integer, ForeignKey('user.id'))
 )
 
+#association_room_vote = Table('room_vote', Base.metadata,
+#    Column('room_id', Integer, ForeignKey('room.id')),
+#    Column('vote_id', Integer, ForeignKey('vote.id'))
+#)
+#
+#association_user_vote = Table('user_vote', Base.metadata,
+#    Column('user_id', Integer, ForeignKey('user.id')),
+#    Column('vote_id', Integer, ForeignKey('vote.id'))
+#)
+
 class Room(Base):
     __tablename__ = 'room'
 
@@ -25,6 +35,7 @@ class Room(Base):
     password = Column(String(20))
     secure = Column(Integer(1))
     user = relationship("User", secondary=association_room_user, backref="room")
+
 
     def __init__(self, name):
 
@@ -39,10 +50,25 @@ class User(Base):
     name = Column(String(255))
     password = Column(String(20))
     login = Column(String(70))
+    perm = relationship("Perm")
 
     def __init__(self, name):
 
         self.name = name
+
+class Perm(Base):
+    __tablename__ = 'perm'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+    def __init__(self, name):
+
+        self.name = name
+
+
+
 
 
 Base.metadata.create_all(engine)
