0a057a2f697f8251d91863938976ba008e292113
Vladimir Kanubrikov <vkanubrikov@dataart.com>
2013-12-16 16:22:27 +0200

    second commit

--- ChatServer/db.py
+++ ChatServer/db.py
@@ -3,8 +3,8 @@
 
 __author__ = 'Vladimir Kanubrikov'
 
-from sqlalchemy import  create_engine, ForeignKey
-from sqlalchemy import  Column, Date, Integer, String
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import Column, Date, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('sqlite:///C:\\sqlitedbs\\chatTest.db', echo=True)
