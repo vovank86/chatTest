43f28fe7bce19ffe35eb9697aefce74bd30e2ca5
Vladimir Kanubrikov <vkanubrikov@dataart.com>
2014-01-22 18:22:41 +0200

    refactor code and add UI functionality

---
+++ ChatClient/UI.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'vkanubrikov'
+
+from Tkinter import *
+import client
+
+
+class LoginForm:
+    def __init__(self):
+
+        """
+
+        @rtype : object
+        """
+        self.lf = Frame(root)
+        self.lf.pack()
+        self.lab1 = Label(self.lf, text="Login:", font="Arial 18")
+        self.user_name = Entry(self.lf, width=20, bd=1)
+        self.lab2 = Label(self.lf, text="Password:", font="Arial 18")
+        self.password = Entry(self.lf, width=20, bd=1)
+        self.button_ok = Button(self.lf, text="OK", width=30, height=5)
+        self.button_ok.bind("<Button-1>", self.send_data)
+        self.lab1.pack()
+        self.user_name.pack()
+        self.lab2.pack()
+        self.password.pack()
+        self.button_ok.pack()
+
+    def send_data(self, event):
+        user_data = {"operation": "login", "user": self.user_name.get(), "password": self.password.get()}
+        server_answer = client.connect(user_data)
+        if not server_answer:
+            root.destroy()
+        else:
+            self.lf.pack_forget()
+            chat = ChatOpen(server_answer)
+
+
+
+class ChatOpen():
+    def __init__(self, chat_data):
+
+        """
+
+        @rtype : object
+        """
+       # data = client.base64.b64decode(chat_data)
+       # data = client.json.loads(data)
+
+        self.chat = Frame(root)
+        self.chat.pack()
+        self.text = Text(self.chat)
+        self.text.pack()
+
+        self.text.bind("<Return>", self.text.insert(END, chat_data+'\n'))
+
+root = Tk()
+obj = LoginForm()
+root.mainloop()
--- ChatClient/client.py
+++ ChatClient/client.py
@@ -6,49 +6,23 @@ __author__ = 'Vladimir Kanubrikov'
 import socket
 import base64
 import json
-from Tkinter import *
+
 
 def connect(user_data):
+    """
+
+    @rtype : object
+    """
     json_encoded = json.dumps(user_data)
     user_data = base64.b64encode(json_encoded)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(('localhost', 9090))
     print sock.recv(1024)
     sock.send(user_data)
-    print sock.recv(1024)
+    data = sock.recv(1024)
     sock.close()
-    #print data
-    #if "fail" == data:
-    #    return False
-    #else:
-    #    return data
 
-
-class LoginForm:
-    def __init__(self):
-        self.lab1 = Label(root, text = "Login:", font = "Arial 18")
-        self.uname = Entry(root, width = 20, bd = 1)
-        self.lab2 = Label(root, text = "Password:", font = "Arial 18")
-        self.password = Entry(root, width = 20, bd = 1)
-        self.butOk = Button(root, text = "OK", width = 30, height = 5)
-        self.butOk.bind("<Button-1>", self.send_data)
-        self.lab1.pack()
-        self.uname.pack()
-        self.lab2.pack()
-        self.password.pack()
-        self.butOk.pack()
-
-    def send_data(self, event):
-        userData = {"operation":"login", "user": self.uname.get(), "password": self.password.get()}
-        if(connect(userData)== False):
-            root.destroy()
-
-root = Tk()
-obj = LoginForm()
-root.mainloop()
-
-
-#userData = {"user":"root", "password":"test"}
-
-
-
+    if "fail" == data:
+        return False
+    else:
+        return data
