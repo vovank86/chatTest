26c2fa2273049361dd68d5c1daed965b45d8a929
vovank86 <vovank86@gmail.com>
2014-02-05 23:09:50 +0200

    another way add settings

---
+++ ChatServer/settings.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Vladimir Kanubrikov'
+
+#******************************
+# Tcp Chat server settings
+#******************************
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'sqlite://',
+        'ROUTE': '/C:\\sqlitedbs\\',
+        'NAME': 'chatTest.db',
+    },
+    'unix': {
+        'ENGINE': 'sqlite://',
+        'ROUTE': '~/chatTest/db/',
+        'NAME': 'chatTest.db',
+    }
+}
+
+SERVER_SOCKET = {
+    'BUFFER_SIZE': 4096,
+    'HOST': '0.0.0.0',
+    'PORT': 9090,
+}
+
+import sys
+print sys.platform
